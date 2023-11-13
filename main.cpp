ccbd70df2e97e1d5e71a71f846b3ab9da9d6113a
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2023-11-12 22:35:00 -0500

    Implementing base algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
+#include <iomanip>
+#include "find_max_product.h"
 int main() {
-    std::cout << "Hola Mundo" << std::endl;
-    return 0;
+    // Caso de uso #1
+    std::vector<int> v {-1000, -1, 2, 3, 4, 150, -900, 100};
+    auto result = find_max_product(v);
+    std::cout << result << std::endl; // Salida esperada: 900000
+
+    // Caso de uso #2
+    std::deque<double> d {1500, -1, 2, 3, 4, -100, 10, -900, 100};
+    auto result2 = find_max_product(d);
+    std::cout << std::fixed << std::setprecision(2) << result2 << std::endl; // Salida esperada: 150000
 }
--- src/find_max_product.h
+++ src/find_max_product.h
@@ -5,6 +5,66 @@
 #ifndef PROG3_PC2_V2023_2_ADDON_FIND_MAX_PRODUCT_H
 #define PROG3_PC2_V2023_2_ADDON_FIND_MAX_PRODUCT_H
 
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include <thread>
+#include <type_traits>
+#include <iostream>
 
+
+template <typename Container>
+auto find_max_product(const Container& c) -> typename Container::value_type {
+    using ValueType = typename Container::value_type;
+
+    // Caso especial: Contenedor vacío
+    if (c.empty()) return 0;
+
+    // Caso especial: Contenedor con un solo elemento
+    if (c.size() == 1) return *c.begin();
+
+    // Caso especial: std::list o std::forward_list, o menos de 10,000 elementos
+    if (std::is_same<Container, std::list<ValueType>>::value ||
+        std::is_same<Container, std::forward_list<ValueType>>::value ||
+        c.size() < 10000) {
+
+        // Inicializamos los valores máximos y mínimos
+        ValueType max1 = std::numeric_limits<ValueType>::min();
+        ValueType max2 = max1;
+        ValueType min1 = std::numeric_limits<ValueType>::max();
+        ValueType min2 = min1;
+
+        // Recorremos el contenedor para encontrar los dos valores más grandes y los dos más pequeños
+        for (const auto& value : c) {
+            if (value > max1) {
+                max2 = max1;
+                max1 = value;
+            } else if (value > max2) {
+                max2 = value;
+            }
+
+            if (value < min1) {
+                min2 = min1;
+                min1 = value;
+            } else if (value < min2) {
+                min2 = value;
+            }
+        }
+
+        // Comparamos los productos de los dos mayores y los dos menores
+        return std::max(max1 * max2, min1 * min2);
+
+
+
+    }
+
+    // caso concurrente
+
+    // Retorna un valor ficticio, reemplazar con la lógica de procesamiento concurrente
+    return ValueType{};
+}
 
 #endif //PROG3_PC2_V2023_2_ADDON_FIND_MAX_PRODUCT_H
